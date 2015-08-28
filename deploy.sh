cd1e73c00a8897d0b31a3b5b5b52beee8ca53b23
Nicolas Dufreche <n.dufreche@gmail.com>
2015-08-28 13:48:33 +0000

    update deploy system

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 set -e # exit with nonzero exit code if anything fails
 
-# clear and re-create the out directory
-rm -rf out || exit 0;
-mkdir out;
+jekyll build
+cp robot.txt _site/
 
-# run our compile script, discussed above
-./compile.sh
+# go to the build directory and create a *new* Git repo
+cd _site
 
-# go to the out directory and create a *new* Git repo
-cd out
 git init
-
 # inside this git repo we'll pretend to be a new user
 git config user.name "Travis CI"
 git config user.email "travis@tiink.it"
@@ -25,4 +21,4 @@ git commit -m "Deploy to GitHub Pages"
 # repo's gh-pages branch. (All previous history on the gh-pages branch
 # will be lost, since we are overwriting it.) We redirect any output to
 # /dev/null to hide any sensitive credential data that might otherwise be exposed.
-git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+#git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1.
